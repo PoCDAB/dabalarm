bf417eeb29d081792df8aea92509bc630508fcd2
Bastiaan Teeuwen <bastiaan@mkcl.nl>
2021-10-12 23:19:12 +0200

    clean up ensemble.sh

--- ensemble.sh
+++ ensemble.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
 #
-# Script to start broadcasting the ensemble
+# Script to start the ensemble broadcast
 #
-
-# channel configuration
-#CH1_FIFO='/tmp/odrdab-ch1.fifo'
-#CH1_PID='/tmp/odrdab-ch1.pid'
-#CH1_PORT=9001
-
-#trap 'kill $(jobs -pr); pkill ffmpeg; rm -f '"$CH1_FIFO $CH1_PID"'; kill $(cat '$CH1_PID')' SIGINT SIGTERM EXIT
 
 # pipe the ETI frames into dablin-gtk and open it locally
 function dablin_stream {
@@ -32,30 +25,6 @@ function named_pipe_stream {
 
 	# TODO remove /tmp/u8.fifo
 }
-
-# create a named pipe and PID file for the sub-channel
-#mkfifo $CH1_FIFO
-#touch $CH1_PID
-
-# start encoding audio
-#./ODR-AudioEnc/odr-audioenc \
-#	--input=$CH1_FIFO \
-#	--format=raw \
-#	--fifo-silence \
-#	--bitrate=128 \
-#	--output=tcp://localhost:9001 \
-#	--pad-socket=ch1 \
-#	--pad=58 &
-
-# start encoding DLS and slideshow information and send it to the audio encoder
-#./ODR-PadEnc/odr-padenc \
-#	--dls=dls/ch1.txt \
-#	--dir=slides/ch1 \
-#	--output=ch1 \
-#	--sleep=5 &
-
-# Restore audio, dls and slides to regular audio channel
-#./ctl.sh ch1 normal
 
 # TODO do a little getopt magic here
 dablin_stream
